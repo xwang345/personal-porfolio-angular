843f901e8d26dfbcac87c3927ef0fd6d08b37414
xwang345 <ryan.wang23@hotmail.com>
2020-01-06 04:42:10 -0500

    change sth

--- angular-app/server.js
+++ angular-app/server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(__dirname + "/angular-app/dist"));
+app.use(express.static(__dirname + "/dist"));
 
 app.listen(process.env.PORT || 8080);
 
 app.get("/*", function (req, res) {
   res.sendFile(
-    path.join(__dirname + "/angular-app/dist/personal-portfolio-angular/index.html")
+    path.join(__dirname + "/dist/personal-portfolio-angular/index.html")
   );
 });
 
-
+console.log("Application is running!!!");
