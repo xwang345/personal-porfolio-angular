671f016c08e86c10b0ff1cf8f9a9769bae6007c7
xwang345 <ryan.wang23@hotmail.com>
2020-01-06 08:02:47 -0500

    change sth

--- src/app/pages/introduction-page/introduction-page.component.ts
+++ src/app/pages/introduction-page/introduction-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-introduction-page',
   templateUrl: './introduction-page.component.html',
-  styleUrls: ['./introduction-page.component.scss']
+  styleUrls: ['./introduction-page.component.css']
 })
 export class IntroductionPageComponent implements OnInit {
   constructor() {}
