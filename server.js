83b1cb3a9ebcdcd222c9acae2246348b1b4bafba
xwang345 <ryan.wang23@hotmail.com>
2020-01-06 05:29:29 -0500

    change sth

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist/personal-portfolio-angular"));
 
 app.listen(process.env.PORT || 8080);
 
